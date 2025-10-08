06477af17d3871ab91be6960b1c136a6a2824b8f
annastasya15 <apriliaannastasya_2327240101@mhs.mdp.ac.id>
2025-10-08 10:50:33 +0700

    commit code persegi pamjang

--- dart_persegi_panjang/lib/dart_persegi_panjang.dart
+++ dart_persegi_panjang/lib/dart_persegi_panjang.dart
@@ -1,3 +1,19 @@
+import 'dart:io';
+import 'dart:math';
+
 double hitungLuas(double panjang, double lebar) {
   return panjang * lebar;
+}
+
+void main() {
+  print('=== Program Hitung Lingkaran ===');
+  stdout.write('Masukkan jari-jari: ');
+  double r = double.parse(stdin.readLineSync()!);
+
+  double luas = pi * pow(r, 2);
+  double keliling = 2 * pi * r;
+
+  print('\n=== Hasil ===');
+  print('Luas lingkaran: ${luas.toStringAsFixed(2)}');
+  print('Keliling lingkaran: ${keliling.toStringAsFixed(2)}');
 }
